00bd79fa9f254d74b714d741a6d503ea55f76b75
Michael Filonenko <filonenko.mikhail@gmail.com>
2012-01-13 22:02:01 +0300

    disable lapack

--- share/lapack/blas-package.asd
+++
@@ -1,9 +0,0 @@
-;;; -*- Mode: lisp -*-
-;;;
-
-(asdf:defsystem blas-package
-  :description "Package definition for BLAS"
-  :components
-  ((:module "blas"
-    :components
-    ((:file "blas-package")))))
--- share/lapack/load-lapack.lisp
+++
@@ -1,8 +0,0 @@
-(in-package #-gcl #:maxima #+GCL "MAXIMA")
-
-;;(pushnew (merge-pathnames "lapack/" *maxima-sharedir*) asdf:*central-registry*)
-
-(pushnew (concatenate 'string  *maxima-sharedir* "/lapack/") asdf:*central-registry*)
-
-(let ((*READ-DEFAULT-FLOAT-FORMAT* 'double-float)) ;; maxima use double-float
-  (asdf:load-system :lapack-interface))
